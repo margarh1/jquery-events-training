4d895fc543efde0a3b1ef8ed504ecd98ce11c72f
margarh1 <ha.margaret1@gmail.com>
2016-10-28 13:32:43 -0700

    prevented the page from refreshing

--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -4,6 +4,13 @@ var rick_roll_youtube_embed_code = "<iframe width='420' height='315' src='https:
 
 $(document).ready(function(){
 
+  $('form').on('click', function handleClick(event) {
+    event.preventDefault();
+    
+  })
+
   $("#time").text( Date.now() );
 
 })
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
