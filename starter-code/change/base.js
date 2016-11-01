a487d5e8342c7df5b884a7c36001f46c013fcb4c
margarh1 <ha.margaret1@gmail.com>
2016-10-31 17:48:54 -0700

    solved change and bonus, technically

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,28 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  var numOne;
+  var numTwo;
+
+  $('.add').on('focusout', function() {
+    if ($(this).prop('valueAsNumber')) {
+      if ($(this).prop('id') === 'left') {
+        numOne = $(this).prop('valueAsNumber');
+      } else if ($(this).prop('id') === 'right') {
+        numTwo = $(this).prop('valueAsNumber');
+      }
+    }
+    if ((numOne !== undefined) && (numOne !== 0) && (numTwo !== undefined) && (numTwo !== 0)) {
+      $('#total').prop('valueAsNumber', numOne + numTwo);
+      console.log($('#total').prop('valueAsNumber'));
+    };
+  });
+
+  $('button').on('click', function() {
+    $('input').prop('valueAsNumber', null);
+    var numOne = null;
+    var numTwo = null;
+  });
+
 })
+  
