3ab038149bb747765f6871927ac4b693e03cc992
margarh1 <ha.margaret1@gmail.com>
2016-10-31 18:13:59 -0700

    finished click and bonus

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,10 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $('span').on('click', function() {
+    
+    var toBeAppended = '<li>You Clicked: ' + $(this).text() + ' at ' + Date.now();
+    $('ul').append(toBeAppended);
+  })
+
 })
