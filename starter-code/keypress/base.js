eced1ceda804b324b234ac4ea0fc1179aa241211
margarh1 <ha.margaret1@gmail.com>
2016-10-31 18:24:48 -0700

    solved keypress

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -2,4 +2,19 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  var currentlyRunning = false;
+  var startTime;
+  var endTime;
+
+  $(document).on('keypress', function() {
+    if (currentlyRunning === false) {
+      startTime = Date.now();
+      currentlyRunning = true;
+    } else {
+      endTime = Date.now();
+      currentlyRunning = false;
+      var secondsTotal = ((endTime - startTime)/1000).toFixed(2) + ' seconds';
+      $('#total-time').text(secondsTotal);
+    }
+  })
 })
